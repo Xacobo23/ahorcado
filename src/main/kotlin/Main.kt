74568cab20ea236e88ef16bacff1addcef62e189
dam1 - Barrera Cabodevila <A23XacoboBC@sanclemente.local>
2024-01-10 10:39:41 +0100

    pierm

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+import kotlin.random.Random
+
+fun main() {
+    var caso = 1
+    val frutas = arrayOf("MANZANA","PERA","NARANJA","HIGO","COCO","PIMIENTO","LIMON","MANGO","UVA","NUEZ","AVELLANA")
+    //poñer o numero
+    //val fruta = frutas[random].split("")
+    //val caracteres = fruta.lenght()
+    do{
+        DibujoAhorcado.dibujar(caso)
+        if()
+        caso = caso+1
+        println(frutas[1])
+    }while(caso<8)
+}
